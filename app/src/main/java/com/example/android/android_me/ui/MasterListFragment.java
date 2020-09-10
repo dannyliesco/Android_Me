c916d76e7f59d08cb0869293094387a83b17c900
Hogen <dannyliesco@gmail.com>
2020-09-10 17:37:40 +0800

    update to use androidx

--- app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -16,12 +16,15 @@
 
 package com.example.android.android_me.ui;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.android.android_me.R;
 import com.example.android.android_me.data.AndroidImageAssets;
@@ -31,12 +34,26 @@ import com.example.android.android_me.data.AndroidImageAssets;
 // The list appears as a grid of images
 public class MasterListFragment extends Fragment {
 
+    OnImageClickListener mCallBack;
     // TODO (1) Define a new interface OnImageClickListener that triggers a callback in the host activity
         // The callback is a method named onImageSelected(int position) that contains information about
         // which position on the grid of images a user has clicked
+    public interface OnImageClickListener{
+        void onImageSelected(int position);
+    }
 
     // TODO (2) Override onAttach to make sure that the container activity has implemented the callback
 
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try{
+            mCallBack = (OnImageClickListener)context;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
     // Mandatory empty constructor
     public MasterListFragment() {
@@ -60,7 +77,12 @@ public class MasterListFragment extends Fragment {
         gridView.setAdapter(mAdapter);
 
         // TODO (3) Set a click listener on the gridView and trigger the callback onImageSelected when an item is clicked
-
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mCallBack.onImageSelected(position);
+            }
+        });
         // Return the root view
         return rootView;
     }
