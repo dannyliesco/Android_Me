043144667e43c921b4e2483ccc0b2ba58951282d
Hogen <dannyliesco@gmail.com>
2020-09-10 14:17:12 +0800

    update to use androidx

---
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.android.android_me.R;
+
+/**
+ * @author Hogen.Xue
+ * @version 1.0.0
+ * @time 2020/9/10 11:41
+ * @brief
+ **/
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
